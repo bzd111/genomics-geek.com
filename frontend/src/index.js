d646e6e09f4cfd982448ded201174f03d6381455
Michael A. Gonzalez <genomics.geek.04.22@gmail.com>
2018-12-09 19:07:58 -0500

    Updated project to use latest cookiecutter template

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import registerServiceWorker from './registerServiceWorker'
+import Raven from 'raven-js'
 
 import 'semantic-ui-css/semantic.min.css'
 import './index.css'
 
-import App from 'App'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
+
+if (process.env.NODE_ENV === 'production') {
+  Raven.config(process.env.REACT_APP_SENTRY_URL).install()
+}
 
 const rootElement = document.getElementById('root')
 
 ReactDOM.render(<App />, rootElement)
-
 if (module.hot && process.env.NODE_ENV === "development") {
   module.hot.accept('./App', () => {
-    const NextApp = require('./App').default
-    ReactDOM.render(<NextApp />, rootElement)
+    const NextRoot = require('./App').default
+    ReactDOM.render(<NextRoot />, rootElement)
   })
 }
 
-// registerServiceWorker()
+serviceWorker.unregister()
